70e72868316a0a91916f8867bb7b790cdc74bf75
Ruslan <m3mpm@users.noreply.github.com>
2025-02-17 18:52:41 +0700

    get args data

--- src/main/java/org/m3mpm/Main.java
+++ src/main/java/org/m3mpm/Main.java
@@ -4,5 +4,12 @@ package org.m3mpm;
 public class Main {
     public static void main(String[] args) {
 
+        if (args.length >= 0) {
+            for (String arg : args) {
+                System.out.println(arg);
+            }
+        }
+
+        System.out.println("args.length = " + args.length);
     }
 }
